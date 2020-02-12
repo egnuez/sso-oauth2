5061c05c367b9e9bb62b9e31a98a110515c1e3ff
Emiliano Nunez <nunez.emiliano@gmail.com>
2020-02-12 11:29:55 -0300

    Add users apps relationship

--- users/models.py
+++ users/models.py
@@ -1,10 +1,20 @@
 from django.db import models
+from django.utils.text import slugify
 
 # Create your models here.
+
+class Apps(models.Model):
+    name = models.CharField(max_length=64)
 
 class Users(models.Model):
     username = models.CharField(max_length=64)
     password = models.CharField(max_length=64)
+    app = models.ManyToManyField(Apps)
 
-class Apps(models.Model):
-    name = models.CharField(max_length=64)
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.username)
+        super(Users, self).save(*args, **kwargs)
+
+    @property
+    def str_apps(self):
+        return [ app.name for app in self.app.all() ]
